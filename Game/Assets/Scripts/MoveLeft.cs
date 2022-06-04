889bbebbf1ea5b8610026f8f8ccdbc17b695c285
Pallavi Gupta <guptapallavi30@gmail.com>
2022-06-03 17:50:11 -0700

    player moves left correctly

--- Game/Assets/Scripts/MoveLeft.cs
+++ Game/Assets/Scripts/MoveLeft.cs
@@ -10,7 +10,8 @@ namespace PlayerCommand
     {
         private float speed = 2.0f;
         private float yDir;
-        private float xDir;   
+        private float xDir;  
+        private Vector2 deltaVelocity = new Vector2(0.005f, 0.005f); 
          
 
         public void Execute(GameObject gameObject) 
@@ -19,33 +20,53 @@ namespace PlayerCommand
             var rigidBody = gameObject.GetComponent<Rigidbody2D>();
             this.yDir = Physics2D.gravity[1];
             this.xDir = Physics2D.gravity[0];
+            float dy = 0;
+            float dx = 0;
 
             if (rigidBody != null)
             {
 
                 Debug.Log("X " + this.xDir + " Y " + this.yDir);
-                if (this.yDir < 0) 
+                if (this.xDir != 0) // gravity going right/left
                 {
-                    rigidBody.velocity = new Vector2(-this.speed, rigidBody.velocity.y);
-                }
-                else if (this.yDir > 0)
+                    // dx = -this.xDir * speed * deltaVelocity.x;
+                    dy = -this.xDir * speed * deltaVelocity.y;
+                    // this.player.transform.Translate(-xDir * moveVertical * deltaVelocity.y, xDir * moveHorizontal * deltaVelocity.x, 0.0f);
+
+                } 
+                else if (this.yDir != 0)// gravity going up/down
                 {
-                    rigidBody.velocity = new Vector2(this.speed, rigidBody.velocity.y);
-                }
-                else if (this.xDir < 0)
-                {
-                    rigidBody.velocity = new Vector2(rigidBody.velocity.x, this.speed);
-                }
-                else if (this.xDir > 0)
-                {
-                    rigidBody.velocity = new Vector2(rigidBody.velocity.x, -this.speed);
-                }
-                else 
+                    dx = this.yDir * speed * deltaVelocity.x;
+                    // Debug.Log("dx: "+ dx);
+                    // dy = -this.yDir * speed * deltaVelocity.y;
+                    // this.player.transform.Translate(-yDir * moveHorizontal * deltaVelocity.x, -yDir * moveVertical * deltaVelocity.y, 0.0f);
+                }else 
                 {
                     Debug.Log("Err: MoveLeft");
                 }
+                rigidBody.transform.Translate(dx, dy, 0.0f);
+                // if (this.yDir < 0) 
+                // {
+                //     rigidBody.velocity = new Vector2(-this.speed, rigidBody.velocity.y);
+                // }
+                // else if (this.yDir > 0)
+                // {
+                //     rigidBody.velocity = new Vector2(this.speed, rigidBody.velocity.y);
+                // }
+                // else if (this.xDir < 0)
+                // {
+                //     rigidBody.velocity = new Vector2(rigidBody.velocity.x, this.speed);
+                // }
+                // else if (this.xDir > 0)
+                // {
+                //     rigidBody.velocity = new Vector2(rigidBody.velocity.x, -this.speed);
+                // }
+                // else 
+                // {
+                //     Debug.Log("Err: MoveLeft");
+                // }
 
-                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                // gameObject.GetComponent<SpriteRenderer>().flipX = true;
 
             }
         }
