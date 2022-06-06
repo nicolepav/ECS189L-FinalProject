836312e6d256da8e8c6a0889d15f470f8e1c5399
dabhoang <danhoang@ucdavis.edu>
2022-06-06 04:53:54 -0700

    implemented functionality for player to pause the game

---
+++ Game/Assets/Scripts/Pause.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    //When the game is paused, a panel will be displayed to tell the player that the game is currently paused.
+    [SerializeField] GameObject pausedScreen;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //The player can press Q to pause the game
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("BangPause1");
+            PauseGame();
+        }
+
+        //The player can press Z to resume the game
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ResumeGame();
+        }
+        
+    }
+
+    //Pauses and freezes the game
+    public void PauseGame()
+    {
+        pausedScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    //Resumes the game 
+    public void ResumeGame()
+    {
+        pausedScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+}
